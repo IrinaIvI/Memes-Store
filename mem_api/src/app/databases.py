56d67cc7f57c1da15ecb47e4d06f2c8d93cf4609
Ira <ira.ivashko.99@gmail.com>
2024-12-07 19:27:13 +0300

    Инициализация базы данных

--- mem_api/src/app/databases.py
+++ mem_api/src/app/databases.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, Asyn
 from typing import AsyncGenerator
 from app.config import DATABASE_URL
 
-engine = create_async_engine(url=DATABASE_URL, echo=True)
+engine = create_async_engine(url="postgresql+asyncpg://postgres:password@host.docker.internal:5432/mem_db", echo=True)
 async_session = async_sessionmaker(bind=engine, class_=AsyncSession)
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
