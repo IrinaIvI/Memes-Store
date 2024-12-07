aceac5f45e66fc44bc4ee1d1d24572b53391c88a
Ira <ira.ivashko.99@gmail.com>
2024-12-07 15:17:15 +0300

    Установка зависимостей

--- mem_api/migrations/env.py
+++ mem_api/migrations/env.py
@@ -4,30 +4,18 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from src.app.databases import URL
 from src.app.models import Base, Meme
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
-config = context.config
+from src.app.config import DATABASE_URL
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+
+config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
+config.set_main_option("sqlalchemy.url", DATABASE_URL + "?async_fallback=True")
 
-config.set_main_option("sqlalchemy.url", URL + "?async_fallback=True")
 target_metadata = Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline() -> None:
--- mem_store/src/app/config.py
+++ mem_store/src/app/config.py
@@ -1,12 +1,12 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    MINIO_URL: str = "http://localhost:9000"
+    MINIO_URL: str = "https://localhost:9000"
     MINIO_ACCESS_KEY: str = "minioaccesskey"
     MINIO_SECRET_KEY: str = "miniosecretkey"
     MINIO_BUCKET_NAME: str = "memes"
     
-    class Config:
-        env_file = ".env"
+    # class Config:
+    #     env_file = ".env"
 
 settings = Settings()
